d4f2d58c1df63fa63ab762bd049144c2e8916041
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-14 13:36:46 -0400

    Added user model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
+// Bring in mongoose
 const { Schema, model } = require('mongoose');
 
+// Function that validates email
 var valEmail = function() {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email)
 };
 
+// Model for User
 const userSchema = new Schema(
     {
        username: {
@@ -18,15 +21,29 @@ const userSchema = new Schema(
         type: String,
         required: true,
         unique: true,
-        validate: [validateEmail, 'Please enter a valid email address.'],
+        validate: [valEmail, 'Please enter a valid email address.'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address.']
        },
 
-       _id: [
+       thoughts: [
             {
                 type: Schema.Types.ObjectId,
-                ref: Thought,
+                ref: 'Thought',
             },
        ],
+
+       friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+       ],
+
+       friendCount: {
+        type: Number,
+        default: friends.length,
+       }
     },
-)
+);
+
+module.export = userSchema;
