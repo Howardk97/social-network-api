95aacbfa99b7a0641a0fdc48dcf4275468329aac
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-14 12:58:45 -0400

    Got server running

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
+// Packages/mongodb connection
 const express = require('express');
 const db = require('./config/connection');
+
+// Routes, port, express, cwd
+// const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Utilize express and routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes);
+
+// Run server once database is loaded
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server is running on ${PORT} at localhost:${PORT}`)
+    });
+});
+
+
 
